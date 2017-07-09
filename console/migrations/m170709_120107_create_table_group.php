73f02405b3af53141544e5fd17a8459fccaf9c24
祁长恩 <qce7@163.com>
2017-07-09 20:05:19 +0800

    Add table group

---
+++ console/migrations/m170709_120107_create_table_group.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\db\Migration;
+
+class m170709_120107_create_table_group extends Migration
+{
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+        $this->createTable('{{%group}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull()->unique(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ], $tableOptions);
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable('{{%group}}');
+        return true;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170709_120107_create_table_group cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
