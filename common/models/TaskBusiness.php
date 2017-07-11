e68c7ba29a2bfd6e2394cf6408d88337143c8c7d
祁长恩 <qce7@163.com>
2017-07-11 09:08:47 +0800

    Add task business model

---
+++ common/models/TaskBusiness.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: danielqi
+ * Date: 7/11/17
+ * Time: 9:05 AM
+ */
+
+namespace common\models;
+
+
+class TaskBusiness extends Task
+{
+    /*
+     * start
+     * pause
+     */
+    public function start()
+    {
+
+    }
+}
